474a756a1d8d2d85ef6c50d10614e4af022bfe55
dylanndo <62081958+dylanndo@users.noreply.github.com>
2026-04-16 14:50:02 -0400

    chore: boostrap Next.js app

---
+++ app/components/Tagline.tsx
@@ -0,0 +1,7 @@
+export function Tagline() {
+    return (
+        <p className="text-sm text-gray-500">
+            Built for UGC creators who want to their their content like a real business
+        </p>
+    )
+}
